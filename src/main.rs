c60d427120ae65a5c355dd181049363135bf378b
herbichimp <52922526+herbichimp@users.noreply.github.com>
2022-08-01 22:32:54 +0800

    This is the original guessing game code that I started adding to.

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    
+    //println!("The secret number is: {}", secret_number);
+
+    let mut count = 0; 
+    
+    loop {
+        count += 1;
+
+        println!("Please input your guess 1-100.");
+        
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            //.expect("Please type a number!");
+
+        println!("You guessed: {}, this was guess #{}", guess,count);
+
+        match guess.cmp(&secret_number) { 
+            Ordering::Less => println!("Too small!"), 
+            Ordering::Greater => println!("Too big!"), 
+            Ordering::Equal => {
+                println!("You win! It took {} guesses to find it!", count);
+                break;
+            }
+        }
+    }
+}
