c6790a467cc3904d6fb6e81d73056549c1abcfdc
herbichimp <52922526+herbichimp@users.noreply.github.com>
2022-08-06 01:17:26 +0800

    Added '0' to quit option
    It displays how many times you guessed then breaks the loop and finishes with how many guesses were made.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,21 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     
-    //let quit = string::from("q");
+//my attempt to make an option to quit by inputting 'q'
+
+    println!("'0' to QUIT and see results.");
+
+    // let mut quit = String::new();
+    // let quit = io::stdin().read_line(&mut quit); 
+    //     match quit.trim().parse() {
+    //         Ok("q") => continue,
+    //         Err("q") => return("game quit."),
+    //     };
+    // fn quit
+
+
+
+
 
     let secret_number = rand::thread_rng().gen_range(1,101);
     
@@ -13,13 +27,17 @@ fn main() {
 
     let mut count = 0; 
     
+    let quit: &str = "q";
 
     loop {
+        
+
+
         count += 1;
         if count == 15 {
             break println!("GAME OVER- YOU LOSE! OUT OF GUESSES.");
         }
-        println!("Please input your guess of 1-100. You can only have 15 guesses!");
+        println!("Please input your guess of 1-100. You can only guess 15 times!");
         
         let mut guess = String::new();
         
@@ -30,7 +48,12 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-            //.expect("Please type a number!");
+                                                                                    //match is better error handling than .expect("Please type a number!"); 
+
+        if guess == 0 {
+            println!("You guessed {} times.",count);
+            break;
+        };
 
         println!("You guessed: {}, this was guess #{}", guess,count);
 
